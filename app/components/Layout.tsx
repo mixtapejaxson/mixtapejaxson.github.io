44c1f9adbcdd8b75d41e1c3f451b006f85011932
Jaxson Waayers <jwaayers5656@gmail.com>
2025-10-16 16:38:20 -0700

    Update Layout.tsx to add banner bar support.

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -6,8 +6,20 @@ interface LayoutProps {
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const bannerText = process.env.BANNER_TEXT;
+  const bannerBgColor = process.env.BANNER_BG_COLOR || "bg-blue-500";
+  const bannerTextColor = process.env.BANNER_TEXT_COLOR || "text-white";
+  const bannerTextPosition = process.env.BANNER_TEXT_POSITION || "text-center";
+
   return (
     <>
+      {bannerText && (
+        <div
+          className={`${bannerBgColor} ${bannerTextColor} ${bannerTextPosition} py-2`}
+        >
+          {bannerText}
+        </div>
+      )}
       <Navigation />
       <main className="pt-16">{children}</main>
       <Footer />
