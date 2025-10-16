08589ec36366786ee5f1aa875bee78c1ca2f4aab
Jaxson Waayers <jwaayers5656@gmail.com>
2025-10-16 16:42:57 -0700

    update layout.tsx to allow for banners to be dismissed.

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -6,18 +6,46 @@ interface LayoutProps {
 }
 
 export default function Layout({ children }: LayoutProps) {
+  import { useState, useEffect } from "react";
+  import Cookies from "js-cookie";
+
   const bannerText = process.env.BANNER_TEXT;
   const bannerBgColor = process.env.BANNER_BG_COLOR || "bg-blue-500";
   const bannerTextColor = process.env.BANNER_TEXT_COLOR || "text-white";
   const bannerTextPosition = process.env.BANNER_TEXT_POSITION || "text-center";
 
+  const bannerKey = `${bannerText}-${bannerBgColor}-${bannerTextColor}-${bannerTextPosition}`;
+  const [isBannerVisible, setIsBannerVisible] = useState(true);
+
+  useEffect(() => {
+    const storedBannerKey = Cookies.get("bannerKey");
+    if (storedBannerKey === bannerKey) {
+      setIsBannerVisible(false);
+    } else {
+      setIsBannerVisible(true);
+    }
+  }, [bannerKey]);
+
+  const closeBanner = () => {
+    setIsBannerVisible(false);
+    Cookies.set("bannerKey", bannerKey, { expires: 7 });
+  };
+
   return (
     <>
-      {bannerText && (
+      {bannerText && isBannerVisible && (
         <div
-          className={`${bannerBgColor} ${bannerTextColor} ${bannerTextPosition} py-2`}
+          className={`${bannerBgColor} ${bannerTextColor} ${bannerTextPosition} py-2 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 mx-auto`}
         >
-          {bannerText}
+          <div className="flex justify-between items-center">
+            <span>{bannerText}</span>
+            <button
+              onClick={closeBanner}
+              className="ml-4 text-sm text-gray-300 hover:text-white"
+            >
+              ✕
+            </button>
+          </div>
         </div>
       )}
       <Navigation />
